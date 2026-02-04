47c18b2739aa55151bd472a963805f1813dd6821
wijaya <muhammadwijaya881@gmail.com>
2026-02-04 09:00:48 +0700

    update contact us dan mebambahkan fitur pesan ke email

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -142,61 +142,58 @@
             <img src="{{ asset('storage/images/logo.png') }}" alt="Logo BorosKopi" class="fade-in">
         </div>
 
-        <div class="isolate bg-white px-6 py-24 sm:py-32 lg:px-8">
+        <div class="isolate bg-[#FAF7F0] px-6 py-24 sm:py-32 lg:px-8">
             <div aria-hidden="true"
                 class="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80">
                 <div style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"
-                    class="relative left-1/2 -z-10 aspect-1155/678 w-144.5 max-w-none -translate-x-1/2 rotate-30 bg-linear-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-40rem)] sm:w-288.75">
+                    class="relative left-1/2 -z-10 aspect-1155/678 w-144.5 max-w-none -translate-x-1/2 rotate-30 bg-gradient-to-tr from-[#D8D2C2] to-[#B17457] opacity-40 sm:left-[calc(50%-40rem)] sm:w-288.75">
                 </div>
             </div>
             <div class="mx-auto max-w-2xl text-center">
-                <h2 class="text-4xl font-semibold tracking-tight text-balance text-gray-900 sm:text-5xl">Contact sales</h2>
-                <p class="mt-2 text-lg/8 text-gray-600">Aute magna irure deserunt veniam aliqua magna enim voluptate.</p>
+                <h2 class="text-4xl font-semibold tracking-tight text-balance text-[#4A4947] sm:text-5xl">Contact Us</h2>
+                <p class="mt-2 text-lg/8 text-[#4A4947]/70">Aute magna irure deserunt veniam aliqua magna enim voluptate.
+                </p>
             </div>
-            <form action="#" method="POST" class="mx-auto mt-16 max-w-xl sm:mt-20">
+            <form action="https://api.web3forms.com/submit" method="POST" class="mx-auto mt-16 max-w-xl sm:mt-20">
+                <input type="hidden" name="access_key" value="99c45d23-f116-4687-8ea6-ae389ce3dc26">
                 <div class="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
                     <div>
-                        <label for="first-name" class="block text-sm/6 font-semibold text-gray-900">First name</label>
+                        <label for="first-name" class="block text-sm/6 font-semibold text-[#4A4947]">Full Name</label>
                         <div class="mt-2.5">
                             <input id="first-name" type="text" name="first-name" autocomplete="given-name"
-                                class="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600" />
+                                class="block w-full rounded-md bg-white px-3.5 py-2 text-base text-[#4A4947] outline-1 -outline-offset-1 outline-[#D8D2C2] placeholder:text-[#4A4947]/40 focus:outline-2 focus:-outline-offset-2 focus:outline-[#B17457]"
+                                required>
                         </div>
-                    </div>
-                    <div>
-                        <label for="last-name" class="block text-sm/6 font-semibold text-gray-900">Last name</label>
-                        <div class="mt-2.5">
-                            <input id="last-name" type="text" name="last-name" autocomplete="family-name"
-                                class="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600" />
-                        </div>
-                    </div>
-                    <div class="sm:col-span-2">
+                        <div class="sm:col-span-2">
 
-                    </div>
-                    <div class="sm:col-span-2">
-                        <label for="email" class="block text-sm/6 font-semibold text-gray-900">Email</label>
-                        <div class="mt-2.5">
-                            <input id="email" type="email" name="email" autocomplete="email"
-                                class="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600" />
+                        </div>
+                        <div class="sm:col-span-2">
+                            <label for="email" class="block text-sm/6 font-semibold text-[#4A4947]">Email</label>
+                            <div class="mt-2.5">
+                                <input id="email" type="email" name="email" autocomplete="email"
+                                    class="block w-full rounded-md bg-white px-3.5 py-2 text-base text-[#4A4947] outline-1 -outline-offset-1 outline-[#D8D2C2] placeholder:text-[#4A4947]/40 focus:outline-2 focus:-outline-offset-2 focus:outline-[#B17457]"
+                                    required>
+                            </div>
+                        </div>
+                        <div class="sm:col-span-2">
+
                         </div>
                     </div>
                     <div class="sm:col-span-2">
-
+                        <label for="message" class="block text-sm/6 font-semibold text-[#4A4947]">Message</label>
+                        <div class="mt-2.5">
+                            <textarea id="message" name="message" rows="4"
+                                class="block w-full rounded-md bg-white px-3.5 py-2 text-base text-[#4A4947] outline-1 -outline-offset-1 outline-[#D8D2C2] placeholder:text-[#4A4947]/40 focus:outline-2 focus:-outline-offset-2 focus:outline-[#B17457]"
+                                required></textarea>
+                        </div>
                     </div>
                 </div>
-                <div class="sm:col-span-2">
-                    <label for="message" class="block text-sm/6 font-semibold text-gray-900">Message</label>
-                    <div class="mt-2.5">
-                        <textarea id="message" name="message" rows="4"
-                            class="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"></textarea>
-                    </div>
+                <div class="mt-10">
+                    <button type="submit"
+                        class="block w-full rounded-md bg-[#B17457] px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-xs hover:bg-[#4A4947] focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-[#B17457]">Let's
+                        talk</button>
                 </div>
-        </div>
-        <div class="mt-10">
-            <button type="submit"
-                class="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Let's
-                talk</button>
-        </div>
-        </form>
+            </form>
         </div>
 
     </section>
